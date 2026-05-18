ed0b5a9d48a3dd523eecf730af24f59dabceab30
Emmanuel Munoz <ton-email-github@email.com>
2026-05-18 14:46:45 +0200

    Final API title

--- frontend/android/app/src/main/java/com/cuisineentrevoisins/app/MainActivity.kt
+++ frontend/android/app/src/main/java/com/cuisineentrevoisins/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.monvoisincuisine.app
+package com.cuisineentrevoisins.mobile
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- frontend/android/app/src/main/java/com/cuisineentrevoisins/app/MainApplication.kt
+++ frontend/android/app/src/main/java/com/cuisineentrevoisins/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.monvoisincuisine.app
+package com.cuisineentrevoisins.mobile
 
 import android.app.Application
 import android.content.res.Configuration
